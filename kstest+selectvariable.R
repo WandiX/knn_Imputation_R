ea1177203459e10adec2a834739fdb1fc2eaae26
dustyspace <zedray.chang@gmail.com>
2017-12-15 15:00:49 -0600

    Delete lm
    delete lm

--- kstest+selectvariable.R
+++ kstest+selectvariable.R
@@ -10,20 +10,6 @@ test_of_samedist <-function(dataset, dadj_val){
     }
 }  
   
-#Select instrumental variables which has correlation with the target master variable to perform the estimation
-select_variable <-function (i, dataset){
-    temp <- lm(dataset[,i] ~ dataset[, 1:(i-1)] + dataset[, (i+1):dim(dataset)[2]])
-    pr_tvalue <- temp$coefficients[, 4]
-    list_variable_name <- dataset[0,][-i] #construct a vector of names without no.i
-    for (j in length(pr_tvalue)){
-        variable_list <- c()
-        if (pr_tvalue[j] < 0.05){
-            append(variable_list, list_variable_name[j])
-        }
-    }
-    return (variable_list)
-}
-  
 #Normalization of data
 norml <- function(test){
     rn <- rownames(test)
